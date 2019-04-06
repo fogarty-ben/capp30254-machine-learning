b1cc59232ae0924f02f5ec76b53c4b8ec1c35afb
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-04-06 14:02:31 -0500

    started question1

--- hws/hw1/chicago_crime.py
+++ hws/hw1/chicago_crime.py
@@ -5,4 +5,24 @@ CAPP 30254: Machine Learning for Public Policy (Spring 2019)
 Ben Fogarty
 '''
 
+import pandas as pd
+from sodapy import Socrata
 
+def download_crime_reports(start_year, end_year):
+    '''
+    Imports the crime data from the Chicago open data portal using the SODA API.
+
+    Inputs:
+    start_year (int): the first year to download crime reports from (valid input
+        is 2001-2018)
+    end_year (int): the last year to dowload crime reports from (valid input is
+        2001-2018)
+
+    Returns: pandas dataframe of crime reports from the specified years
+    '''
+    client = Socrata('data.cityofchicago.org', '***REMOVED***')
+    where_clause = 'year between {} and {}'.format(start_year, end_year)
+    results = client.get('6zsd-86xi', where=where_clause)
+    results_df = pd.DataFrame.from_records(results)
+
+    return results_df
