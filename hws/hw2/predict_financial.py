9fd6cfb2253a54e90ef5b62d2041f404582616fb
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-04-15 23:18:06 -0500

    build up go() function

--- hws/hw2/predict_financial.py
+++ hws/hw2/predict_financial.py
@@ -16,10 +16,31 @@ def go(filepath):
     Inputs:
     filepath (str): path to the file containing the training data
     '''
-    df = pl.read_csv(filepath)
+    col_types = {'SeriousDlqin2yrs': float,
+                 'RevolvingUtilizationOfUnsecuredLines': float,
+                 'age': float,
+                 'NumberOfTime30-59DaysPastDueNotWorse': float,
+                 'zipcode': str,
+                 'DebtRatio': float,
+                 'MonthlyIncome': float,
+                 'NumberOfOpenCreditLinesAndLoans': float,
+                 'NumberOfTimes90DaysLate': float,
+                 'NumberRealEstateLoansOrLines': float,
+                 'NumberOfTime60-89DaysPastDueNotWorse': float,
+                 'NumberOfDependents': float}
+    df = pl.read_csv(filepath, col_types=col_types)
+    df = pl.preprocess_data(df)
     explore_data(df)
-    process_data(df)
-    dt, accuracy = build_model(df)
+    df = process_data(df)
+    dt, accuracy = build_eval_model(df)
+    print('Accuracy: {}'.format(accuracy))
+
+    print(df.columns)
+    feature_names = list(df.columns)
+    print(feature_names)
+    feature_names.remove('PersonID')
+    feature_names.remove('SeriousDlqin2yrs')
+    pl.visualize_decision_tree(dt, feature_names, class_names=['0', '1'])
 
 def explore_data(df):
     '''
@@ -29,18 +50,47 @@ def explore_data(df):
     Inputs:
     df (pandas dataframe): the dataframe
     '''
-    pass
+    summary = pl.summarize_data(df)
+
+    pw_corr_vars = ['SeriousDlqin2yrs', 'RevolvingUtilizationOfUnsecuredLines',
+       'age', 'NumberOfTime30-59DaysPastDueNotWorse', 'DebtRatio',
+       'MonthlyIncome', 'NumberOfOpenCreditLinesAndLoans',
+       'NumberOfTimes90DaysLate', 'NumberRealEstateLoansOrLines',
+       'NumberOfTime60-89DaysPastDueNotWorse', 'NumberOfDependents']
+    correlations = pl.pw_correlate(df, variables=pw_corr_vars)
+
+    for var in df.columns:
+        if not var == 'PersonID':
+            print(var)
+            if var in summary.index:
+                print(summary.loc[var, :])
+            pl.show_distribution(df, var).show()
+            if var in pw_corr_vars:
+                print('Correlations with {}'.format(var))
+                print(correlations[var])
+            print()
+
+    print('By Zipcode Summary:')
+    print(pl.summarize_data(df, grouping_vars='zipcode'))
+
+    print('Outlier summary:')
+    outliers = pl.find_outliers(df, excluded=['SeriousDlqin2yrs', 'PersonID'])
+    outliers.sort_values('Count Outlier', ascending=False, inplace=True)
+    print(outliers['Count Outlier'].value_counts())
 
 def process_data(df):
     '''
-    Pre-processes data and generates categorical and binary features.
+    Generates categorical and binary features.
 
     Inputs:
     df (pandas dataframe): the dataframe
 
     Returns: pandas dataframe, the processed dataset
     '''
-    pass
+    df = pl.create_dummies(df, 'zipcode')
+
+    return df
+
 
 def build_eval_model(df):
     '''
@@ -56,7 +106,14 @@ def build_eval_model(df):
 
     For later implementation: splitting data into test and training sets
     '''
-    pass
+    features = df.drop(['PersonID', 'SeriousDlqin2yrs'], axis=1)
+    target = df.SeriousDlqin2yrs
+    dt = pl.generate_decision_tree(features, target)
+
+    accuracy = pl.score_decision_tree(dt, features, target)
+
+    return dt, accuracy
+
 
 if __name__ == '__main__':
     go()
