83322e177973384f700d20577962de992e021ef1
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-04-18 01:31:34 -0500

    ready for pre-submission linting

--- hws/hw2/pipeline_library.py
+++ hws/hw2/pipeline_library.py
@@ -17,33 +17,32 @@ from sklearn import tree
 def read_csv(filepath, cols=None, col_types=None, index_col=None):
     '''
     Imports a CSV file into a pandas data frame, optionally specifying columns
-    to import and the types of the columns.
+    to import, the types of the columns, and an index column.
 
     Inputs:
     filepath (str): the path to the file
-    row_index (str or list of strs): an optional column name or list of column
-        names to index the rows of the dataframe with
     cols (list of strings): an optional list of columns from the data to import;
-        only to be used if the first line of the csv is a header row
+        can only be used if the first line of the csv is a header row
     col_types (dict mapping strings to types): an optional dictionary specifying
         the data types for each column; each key must be a the name of a column
-        in the dataset and the associate value must be a pandas datatype (valid
+        in the dataset and the associated value must be a pandas datatype (valid
         types listed here: http://pandas.pydata.org/pandas-docs/stable/
         getting_started/basics.html#dtypes)
-    index_col (str): optional name of column providing an index for the data
+    index_col (str or list of strs): an optional column name or list of column
+        names to index the rows of the dataframe with
 
     Returns: pandas dataframe
     '''
-    return pd.read_csv(filepath, usecols=cols, dtype=col_types, index_col=index_col)
+    return pd.read_csv(filepath, usecols=cols, dtype=col_types, 
+                       index_col=index_col)
 
 def show_distribution(series):
     '''
-    Graphs a histogram and the box plot of one variable in a
-    dataframe.
+    Graphs a histogram and the box plot of numeric type series and a bar plot
+    of categorial type series.
 
     Inputs:
-    df (pandas series): the variable to show the
-        distribution of as a column
+    df (pandas series): the variable to show the distribution of
 
     Returns: matplotlib figure
     
@@ -72,14 +71,14 @@ def show_distribution(series):
 
 def pw_correlate(df, variables=None, visualize=False):
     '''
-    Calculates a table of pairwise correlations between numberic variables.
+    Calculates a table of pairwise correlations between numeric variables.
 
     Inputs:
     df (pandas dataframe): dataframe containing the variables to calculate
         pairwise correlation between
-    variables (list of strs): the list of variables to calculate pairwise
+    variables (list of strs): optional list of variables to calculate pairwise
         correlations between; each passed str must be name of a numeric type
-        (including booleans) column in the dataframe; default is all numberic
+        (including booleans) column in the dataframe; default is all numeric
         type variables in the dataframe
     visualize (bool): optional parameter, if enabled the function generates
         a heat map to help draw attention to larger correlation coefficients
@@ -144,7 +143,7 @@ def summarize_data(df, grouping_vars=None, agg_cols=None):
 
     return summary.transpose()
 
-def find_oulier_univariate(series, visualize=False):
+def find_ouliers_univariate(series):
     '''
     Identifies values in a series that fall more than 1.5 * IQR below the first
     quartile or 1.5 * IQR above the third quartile.
@@ -164,16 +163,16 @@ def find_oulier_univariate(series, visualize=False):
 def find_outliers(df, excluded=None):
     '''
     Identifies outliers for each numeric column in a dataframe, and returns a
-    data matching each record with the columns for which it is an outlier
-    and the number and percent of numeric columns for which a is an outlier.
+    dataframe matching each record with the columns for which it is an outlier
+    and the number and percent of checked columns for which a is an outlier.
     Outlier is defined as any value thats fall more than 1.5 * IQR below the
     first quartile or 1.5 * IQR above the third quartile of all the values in a
     column.
 
     Inputs:
     df (pandas dataframe): the dataframe to find outliers in
-    excluded (str list of strs): the name or a list of names of columns not to
-        look for outliers in; default is including all numeric columns
+    excluded (str list of strs): optional column name or a list of columns names
+        not to look for outliers in; default is including all numeric columns
 
     Returns: pandas series
     '''
@@ -184,7 +183,7 @@ def find_outliers(df, excluded=None):
 
     outliers = df[numeric_cols]\
                  .drop(excluded, axis=1, errors='ignore')\
-                 .apply(find_oulier_univariate, axis=0)
+                 .apply(find_ouliers_univariate, axis=0)
     outliers['Count Outlier'] = outliers.sum(axis=1, numeric_only=True)
     outliers['% Outlier'] = (outliers['Count Outlier'] /
                              (len(outliers.columns) - 1) * 100)
@@ -194,12 +193,12 @@ def find_outliers(df, excluded=None):
 def replace_missing(series):
     '''
     Replaces missing values in a series with the median value if the series is
-    numeric and with the modal value if the series is non-numeric
+    numeric and with the modal value if the series is non-numeric.
 
     Inputs:
     series (pandas series): the series to replace data in
 
-    Returns (pandas series):
+    Returns (pandas series)
     '''
     if pd.api.types.is_numeric_dtype(series):
         median = np.median(series.dropna())
@@ -211,7 +210,7 @@ def replace_missing(series):
 def preprocess_data(df):
     '''
     Removes missing values, replacing them with the median value in numeric
-    fields and the modal value in non-numeric columns
+    columns and the modal value in non-numeric columns.
 
     Inputs:
     df (pandas dataframe): contains the data to preprocess
@@ -227,9 +226,10 @@ def cut_variable(series, bins, labels=None):
     Inputs:
     series (pandas series): the variable to discretize
     bins (int or list of numerics): the binning rule:
-        - if int: cuts the variable into n  approximately eqipercentile sized 
-          bins; the range of the variable is .1% on the right side to include
-          the maximum valuse
+        - if int: cuts the variable into approximate n bins with an approximately
+          equal number of observations (there may be fewer bins or some bins
+          with a substantially larger number of observations depending on the
+          distribution of the data)
         - if list of numerics: cuts the variable into bins with edges determined
           by the sorted numerics in the list; any values not covered by the
           specified will be labeled as missing
@@ -239,9 +239,11 @@ def cut_variable(series, bins, labels=None):
     Return: pandas series
     '''
     if type(bins) is int:
-        return pd.qcut(series, bins, labels=labels, duplicates='drop').astype(str)
+        return pd.qcut(series, bins, labels=labels, duplicates='drop')\
+                 .astype('category')
 
-    return pd.cut(series, bins, labels=labels, include_lowest=True)
+    return pd.cut(series, bins, labels=labels, include_lowest=True)\
+             .astype('category')
 
 def create_dummies(df, column):
     '''
@@ -253,8 +255,8 @@ def create_dummies(df, column):
         variables to convert to dummy variables; if not specified then any
         colums with dtype object or category are converted
 
-    Returns: pandas dataframe where columns to be converted are replaced with
-        columns contatining dummy variables
+    Returns: pandas dataframe where the columns to be converted is replaced with
+        columns containing dummy variables
     '''
     col = df[column]
     values = list(col.value_counts().index)
@@ -272,17 +274,14 @@ def generate_decision_tree(features, target, dt=None):
     other attributes (features).
 
     Inputs:
-    features (pandas dataframe): the features to build the decision tree with;
-        all columns must be numeric (either the data within them should be
-        numeric or it should be converted from categorical data to dummies)
-    target (pandas series): the target the decision tree is designed to predict;
-        should be categorical data
+    features (pandas dataframe): data for features to build the decision tree 
+        with; all columns must be numeric in type
+    target (pandas series): data for target attribute the decision tree is
+        designed to predict; should be categorical data with a numerial form
     dt (sklearn.tree.DecisionTree): optional DecisionTreeClassifier object so
         the parameters of the DecisionTreeClassifier can be specified; if
         unspecified, a new DecisionTreeClassifier object will be instantiated
-        with all the default arguments, except the function to measure the
-        quality of a split ('entropy' will be specified instead of the default,
-        'gini')
+        with all the default arguments
 
     Returns: sklearn.tree.DecisionTreeClassifier, the trained 
         DecisionTreeClassifier 
@@ -293,7 +292,7 @@ def generate_decision_tree(features, target, dt=None):
         sklearn.tree.DecisionTreeClassifier
     '''
     if not dt:
-        dt = tree.DecisionTreeClassifier(criterion='entropy')
+        dt = tree.DecisionTreeClassifier()
 
     return dt.fit(features, target)
 
@@ -305,12 +304,12 @@ def score_decision_tree(dt, test_features, test_target):
     Inputs:
     dt (sklearn.tree.DecisionTreeClassifier): a trained decision tree classifier
         model
-    test_features (pandas dataframe): the feature values for the observations
-        the decision tree is being tested against; all columns must be numeric
-        (either the data within them should be numeric or it should be converted
-        from categorical data to dummies)
-    target (pandas series): the target attribute values for the observations the
-        the decision tree is being tested against; should be categorical data
+    test_features (pandas dataframe): testing data for the features in the
+        decision tree; structure of the data (columns and column types) must
+        match the data used to train the decision tree
+    target (pandas series): testing data for the target attribute a decision
+        tree is predicting; structure of the data (columns and column types) 
+        must match the data used to train the decision tree
 
     Returns: float
 
@@ -330,8 +329,8 @@ def visualize_decision_tree(dt, feature_names, class_names, filepath='tree'):
     feature_names (list of strs): a list of the features the data was trained
         with; must be in the same order as the features in the dataset
     class_names (list of strs): a list of the classes of the target attribute
-        the model is predicting; must be in the same order as the features in
-        the dataset
+        the model is predicting; must match the target attribute values if
+        those values were given in ascending order
     filepath (str): optitional parameter specifying the output path for the
         visualization (do not include the file extension); default is 'tree' in
         the present working directory
@@ -343,6 +342,6 @@ def visualize_decision_tree(dt, feature_names, class_names, filepath='tree'):
         generated/sklearn.tree.export_graphviz.htm
     '''
     dot_data = tree.export_graphviz(dt, None, feature_names=feature_names, 
-                                  class_names=class_names, filled=True)
+                                    class_names=class_names, filled=True)
     graph = graphviz.Source(dot_data)
     output_path = graph.render(filename=filepath, view=True)
